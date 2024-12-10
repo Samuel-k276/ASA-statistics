b66df08e66d1c077de9301639a2d8c22d1b2f950
samuel <samuel.gomes.k276@gmail.com>
2024-12-10 22:57:08 +0000

    tratamento dos dados dos rankings feito

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 @app.route('/api/statistics/ranking/proj2', methods=['GET'])
 def get_statistics():
    data = [
-      {"group": "al019", "result": "1700", "time": "01:23:45"},
-      {"group": "al001", "result": "1550", "time": "00:45:30"},
+      {"group": "al019", "result": "1750", "time": "01:23:45"},
+      {"group": "al001", "result": "1750", "time": "00:45:30"},
+      {"group": "al011", "result": "1750", "time": "00:65:30"},
+      {"group": "al021", "result": "1450", "time": "00:75:30"},
+      {"group": "al031", "result": "1250", "time": "00:85:30"},
+
    ]
-   return jsonify(data)
+   return jsonify(sort_data(data))
 
 """
 @app.route('/api/statistics/rankings', methods=['GET'])
--- scrape/base.py
+++ scrape/base.py
@@ -25,17 +25,23 @@ def web_scrape(url):
          
          if len(cols) >= 3:  # Garantir que há pelo menos 3 colunas (group, result, time)
             group = cols[0].text.strip()
-            result = float(cols[1].text.strip())  # Pode ser outro tipo de dado dependendo do formato
-            time = float(cols[2].text.strip())  # Pode ser outro tipo de dado dependendo do formato
+            result = cols[1].text.strip()  
+            time = cols[2].text.strip()
             data.append({'group': group, 'result': result, 'time': time})
 
    return data
 
+def time_str_to_int(time) -> int:
+   i_1 = time.find(":")
+   i_2 = time[i_1+1:].find(":")+i_1+1
+   print(i_1, i_2)   
+   total = int(time[:i_1])*3600 + int(time[i_1+1:i_2])*60 + int(time[i_2+1:])
+   return total
 
 # Ordenação dos dados
 def sort_data(data): 
    # Ordena pela chave 'result' decrescente e, em caso de empate, pela chave 'time' crescente
-   return sorted(data, key=lambda x: (-x['result'], x['time']))
+   return sorted(data, key=lambda x: (-int(x['result']), time_str_to_int(x['time'])))
 
 def scrape_all_projects_rankings():
    data = {
