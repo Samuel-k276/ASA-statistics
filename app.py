0e3845828b02d77c515e398d85ab328446bf4563
samuel <samuel.gomes.k276@gmail.com>
2024-12-13 21:54:23 +0000

    comit

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, render_template
 from scrape.base import scrape_2
 import os
+import threading
+import time
 
 app = Flask(__name__)
 
 data = scrape_2()
+
+def continuous_scraping(interval=100):
+   global data
+   while True:
+      try:
+         # Atualiza os dados com o scraping
+         data = scrape_2()
+         print("Data updated successfully.")
+      except Exception as e:
+         print(f"Error during scraping: {e}")
+      time.sleep(interval)
 
 
 # Load Raw Data for Proj2
@@ -20,5 +33,8 @@ def home():
 
 
 if __name__ == "__main__":
+   scraping_thread = threading.Thread(target=continuous_scraping, args=(100,), daemon=True)
+   scraping_thread.start()
+
    port = int(os.getenv("PORT", 5000))
    app.run(debug=True, port=port, host="0.0.0.0")
